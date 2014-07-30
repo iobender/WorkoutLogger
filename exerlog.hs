268473b732030746a48b7008c13d7ae0313c8718
Matthew Bender <me@iobender.com>
2014-07-29 21:39:41 -0700

    Refactored workout heirarchy and added support for more workouts. Yay Haskell data types\!

--- exerlog.hs
+++ exerlog.hs
@@ -1,16 +1,38 @@
 -- Matthew Bender
 --
 -- exerlog.hs: workout logger
-import Data.Time.Clock
+type Time = Int
 
-data Workout = 
-	Run { 
+-- workout types
+-- fields with type wrapped in Maybe are optional
+data Workout = Distance DistanceWorkout | Core CoreWorkout | Weights WeightsWorkout | Sports SportsWorkout deriving Show
+
+data DistanceWorkout = 
+	DistanceWorkout { 
+		dwt :: DistanceWorkoutType,
 		distance :: Float,
-		time :: Int -- implement as an int # of seconds for now
-	} | 
-	Weights {
-		weight :: Int,
-		reps :: Int
-	}
-	deriving (Show)
+		time :: Time
+	} deriving Show
+data DistanceWorkoutType = Run | Bike | Swim | OtherDistance String deriving Show
 
+data CoreWorkout = 
+	CoreWorkout {
+		cwt :: CoreWorkoutType,
+		coreReps
+	} deriving Show
+data CoreWorkoutType = Pushups | Crunches | SideDips | OtherCore String deriving Show
+
+data WeightsWorkout = 
+	WeightsWorkout {
+		wwt :: WeightsWorkoutType,
+		weight :: Int,
+		weightReps :: Int
+	} deriving Show
+data WeightsWorkoutType = Curls | Bench | OtherWeights String deriving Show
+
+data SportsWorkout = 
+	SportsWorkout {
+		swt :: SportsWorkoutType,
+		duration :: Time
+	} deriving Show
+data SportsWorkoutType = Baseball | Soccer | Frisbee | Football | OtherSports String deriving Show
