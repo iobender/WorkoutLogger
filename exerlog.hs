9614c6892dfaba2eb478132ab9fdec25a8c0e8df
Matthew Bender <me@iobender.com>
2014-07-29 21:13:03 -0700

    Added data types for two types of workouts, runs and bikes

--- exerlog.hs
+++ exerlog.hs
@@ -1,3 +1,16 @@
 -- Matthew Bender
 --
 -- exerlog.hs: workout logger
+import Data.Time.Clock
+
+data Workout = 
+	Run { 
+		distance :: Float,
+		time :: Int -- implement as an int # of seconds for now
+	} | 
+	Weights {
+		weight :: Int,
+		reps :: Int
+	}
+	deriving (Show)
+
