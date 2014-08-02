ab2d88cfd93531fb382839f2926d03c307626982
Matthew Bender <me@iobender.com>
2014-08-01 22:50:20 -0700

    'Finished' function to get a Workout from stdin
    getWorkout is now essentially complete and functional.
    Calling it will read a Workout from stdin.
    We say essentially because the whole thing still lacks features on the
    TODO list, but the structure for reading a Workout is complete

--- Main.hs
+++ Main.hs
@@ -1,13 +1,6 @@
 import Workout
 import ParseWorkouts
 
--- test vars
-runW = Distance Run 3.1 17
-run = Workout Hard runW
-
-curlW = Weights Curls 25 15
-curl = Workout Easy curlW 
-
 --for now we will just output the workout that was just entered 
 --(in our format for the workout, of course)
 main :: IO ()
--- ParseWorkouts.hs
+++ ParseWorkouts.hs
@@ -1,43 +1,65 @@
 module ParseWorkouts where
 
 import Data.Char
+import Text.Read
+import System.IO
 import Workout
 
--- fix whitespace and make lowercase
-basestr :: String -> String
-basestr = unwords . words . map toLower
+--lookup ignoring case and whitespace
+lookup' :: String -> [(String, a)] -> Maybe a
+lookup' = lookup . unwords . words . map toLower
 
-lookstr :: String -> [(String, a)] -> Maybe a
-lookstr = lookup . basestr
+getRead :: Read a => String -> IO a
+getRead p = 
+	putStr p >> 
+	hFlush stdout >> 
+	getLine >>= 
+	(\s -> case readMaybe s of
+	 	Nothing -> getRead p
+		Just v -> return v)
 
-difficultyToks :: [(String, Difficulty)]
-difficultyToks = [("easy", Easy), ("medium", Medium), ("hard", Hard)]
+--given an association table, reads stdin until the one of the keys is 
+--entered, ignoring case and whitespace, and returns that value
+getLookup :: String -> [(String, a)] -> IO a
+getLookup p m = 
+	putStr p >> 
+	hFlush stdout >> 
+	getLine >>=
+	(\s -> case lookup' s m of
+	 	Nothing -> getLookup p m 
+		Just v -> return v)
 
+--gets an entire workout from stdin. Main function to be called
 getWorkout :: IO Workout
---placeholder for actual parsing of input
-getWorkout = return $ Workout Easy $ Sports Frisbee 100
+getWorkout = do
+	diff <- getDifficulty
+	wt <- getWorkoutTypeFunc >>= (\f -> f >>= return)
+	return $ Workout diff wt
 
 getDifficulty :: IO Difficulty
-getDifficulty = 
-	getLine >>= 
-	(\s -> case lookstr s difficultyToks of
-	 	Nothing -> getDifficulty
-		Just d -> return d)
-
-workoutTypeFuncToks :: [(String, IO WorkoutType)]
-workoutTypeFuncToks = [("distance", getDistanceWorkout),
-		      ("core", getCoreWorkout),
-	      	      ("weights", getWeightsWorkout),
-		      ("sports", getSportsWorkout)]	      
+getDifficulty = getLookup "Diff: " [("easy", Easy), ("medium", Medium), ("hard", Hard)]
 
 getWorkoutTypeFunc :: IO (IO WorkoutType)
-getWorkoutTypeFunc = 
-	getLine >>=
-	(\s -> case lookstr s workoutTypeFuncToks of
-	 	Nothing -> getWorkoutTypeFunc
-		Just f -> return f)
+getWorkoutTypeFunc = getLookup "type: " [("distance", getDistanceWorkout), ("core", getCoreWorkout), ("weights", getWeightsWorkout), ("sports", getSportsWorkout)]	       
 
-getDistanceWorkout = error "distance not yet implemented"
-getCoreWorkout = error "core not yet implemented"
-getWeightsWorkout = error "weights not yet implemented"
-getSportsWorkout = error "sports not yet implemented"
+getDistanceWorkout = do
+	dwt <- getLookup "dist type: " [("run", Run), ("bike", Bike), ("swim", Swim)]
+	distance <- getRead "distance: "
+	time <- getRead "time: "
+	return $ Distance dwt distance time
+
+getCoreWorkout = do
+	cwt <- getLookup "core type: " [("pushups", Pushups), ("crunches", Crunches), ("sidedips", SideDips)]
+	coreReps <- getRead "reps: "
+	return $ Core cwt coreReps
+
+getWeightsWorkout = do
+	wwt <- getLookup "weights type: " [("curls", Curls), ("bench", Bench)]
+	weight <- getRead "weight: "
+	weightReps <- getRead "weightReps"
+	return $ Weights wwt weight weightReps
+
+getSportsWorkout = do
+	swt <- getLookup "sports type: " [("baseball", Baseball), ("soccer", Soccer), ("frisbee", Frisbee), ("football", Football)]
+	duration <- getRead "duration: "
+	return $ Sports swt duration
